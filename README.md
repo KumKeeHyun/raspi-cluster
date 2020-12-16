775cb80ecc2f03624ef08b067ac8957bbd0b845d
KumKeeHyun <kbzjung359@gmail.com>
2020-12-16 23:46:11 +0900

    init kubernetes

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # raspi-cluster
-클러스터 환경(쿠버네티스, 카프카 엘라스틱서치 클러스터, 하둡 생태계) 등을 공부하는 레포
+Raspberry pi 4 를 이용해서 클러스터 환경(쿠버네티스, 카프카 엘라스틱서치 클러스터, 하둡 생태계)을 공부하는 레포
+
+## OS
+Ubuntu Server 20.04.1 LTS
+- https://ubuntu.com/download/raspberry-pi
+
+## Component
+![image](https://user-images.githubusercontent.com/44857109/102356152-32af9200-3ff0-11eb-8c17-2e546cf1754a.png){: width="60%" height="60%"}
+
+- Raspberry pi 4 - RAM 8Gb 3개
+- Raspberry pi 4 - RAM 4Gb 1개
+- ipTime T5004 (2Gbps)
---
+++ kubernetes/setup.sh
@@ -0,0 +1,52 @@
+#!/bin/sh
+
+DOCKER_VER=5:19.03.12~3-0~ubuntu-focal
+K8S_VER=1.20.0-00
+
+sudo apt update
+sudo apt upgrade
+
+# set docker pakage path
+sudo apt-get install apt-transport-https ca-certificates curl gnupg-agent software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=arm64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+sudo apt-get update
+
+# install docker specific version 
+sudo apt-get install docker-ce=${DOCKER_VER} docker-ce-cli=${DOCKER_VER} containerd.io
+
+# set docker cgroupdriver cgroupfs -> systemd
+cat <<EOF | sudo tee /etc/docker/daemon.json
+{
+  "exec-opts": ["native.cgroupdriver=systemd"],
+  "log-driver": "json-file",
+  "log-opts": {
+    "max-size": "100m"
+  },
+  "storage-driver": "overlay2"
+}
+EOF
+
+sudo mkdir -p /etc/systemd/system/docker.service.d
+sudo systemctl daemon-reload
+sudo service docker restart
+
+# set kubernetes pakage path and some configuration
+sudo apt-get update && sudo apt-get install -y apt-transport-https curl
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+
+cat <<EOF | sudo tee /etc/apt/sources.list.d/kubernetes.list
+deb https://apt.kubernetes.io/ kubernetes-xenial main
+EOF
+
+cat <<EOF | sudo tee /etc/sysctl.d/k8s.conf
+net.bridge.bridge-nf-call-ip6tables = 1
+net.bridge.bridge-nf-call-iptables = 1
+EOF
+
+sudo sysctl --system
+sudo apt-get update
+
+# install kubernetes specific version 
+sudo apt-get install -y kubelet=${K8S_VER} kubeadm=${K8S_VER} kubectl=${K8S_VER}
+sudo apt-mark hold kubelet kubeadm kubectl
