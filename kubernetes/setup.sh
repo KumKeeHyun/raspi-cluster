7394b4fc998235e8dda69430117b7d0791021064
KumKeeHyun <kbzjung359@gmail.com>
2020-12-29 13:02:13 +0900

    Update setup.sh

--- kubernetes/setup.sh
+++ kubernetes/setup.sh
@@ -4,10 +4,10 @@ DOCKER_VER=5:19.03.12~3-0~ubuntu-focal
 K8S_VER=1.20.0-00
 
 sudo apt update
-sudo apt upgrade
+sudo apt -y upgrade
 
 # set docker pakage path
-sudo apt-get install apt-transport-https ca-certificates curl gnupg-agent software-properties-common
+sudo apt-get install -y apt-transport-https ca-certificates curl gnupg-agent software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=arm64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 sudo apt-get update
